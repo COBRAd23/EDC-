0e5274c9c8c78e41759c62f9e3836d64a7333c2e
cobrad23 <cobradesign@outlook.com>
2026-05-11 20:41:53 -0300

    edcarborismo

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -19,7 +19,7 @@ const Contacto = () => {
     const data = Object.fromEntries(formData.entries());
 
     try {
-      const response = await fetch("https://formsubmit.co/ajax/cobradesign@outlook.com", {
+      const response = await fetch("https://formsubmit.co/ajax/edcarborismo@gmail.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
