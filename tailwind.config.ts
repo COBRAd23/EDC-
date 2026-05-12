286e4931e6cb22023b9d7bbbebc039ce1fad6bd8
cobrad23 <cobradesign@outlook.com>
2026-05-11 21:15:25 -0300

    adapatdo al 15 pulgadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,20 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: "1.5rem",
-      screens: { "2xl": "1536px" },
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "2rem",
+        lg: "2.5rem",
+        xl: "3rem",
+        "2xl": "4rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
     },
     extend: {
       fontFamily: {
